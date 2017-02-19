54301bb31e77f10a86314de4fea44f875f377ea6
Andrew Brown <andrew@monsterboxpro.com>
2017-02-19 17:03:38 -0500

    remove old ordering in pagination

--- app/queries/pagination/offset.sql
+++ app/queries/pagination/offset.sql
@@ -1,12 +1,4 @@
 {{#unless count}}
-  {{#if order}}
-    ORDER BY {{order}}
-    {{#if order_dir}}
-      {{order_dir}}
-    {{else}}
-      ASC
-    {{/if}}
-  {{/if}}
   LIMIT {{per_page}}
   {{#if page}}
     OFFSET (({{page}} - 1) * {{per_page}})
--- lib/monster_queries/version.rb
+++ lib/monster_queries/version.rb
@@ -1,3 +1,3 @@
 module MonsterQueries
-  VERSION = "1.2.13"
+  VERSION = "1.2.14"
 end
